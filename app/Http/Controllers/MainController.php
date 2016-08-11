2573ad247e5f67da89eb98ff5e4f49018ec7d2f7
Andrey <Andreyka@Andreykas-Mac.local>
2016-08-11 15:15:51 +0300

    initial commit

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,16 @@
+<?php
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Response;
+
+/**
+ * Основной контроллер
+ *
+ * @author Андрей Инихов <Andrey.Inihov@yandex.ru>
+ * @version 0.1
+ */
+class CIController extends Controller
+{
+
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+Test for Get Intent company
